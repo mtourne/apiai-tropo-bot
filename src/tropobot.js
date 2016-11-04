e4a1a4697a26c3b197f0b9ed5e3d0ace6e5f16c9
Matthieu Tourne <matthieu.tourne@gmail.com>
2016-11-04 13:35:46 -0700

    Adding a new webapi command to initiate to be able to initiate a dialoge from tropo

--- src/tropobot.js
+++ src/tropobot.js
@@ -4,6 +4,9 @@ const apiai = require('apiai');
 const uuid = require('node-uuid');
 const request = require('request');
 
+// change to turn on debug
+const devmode = true;
+
 module.exports = class TropoBot {
 
     get apiaiService() {
@@ -42,58 +45,125 @@ module.exports = class TropoBot {
     }
 
     processMessage(req, res) {
-        if (this._botConfig.devConfig) {
-            console.log("body", req.body);
+      if (this._botConfig.devConfig || devmode) {
+        console.log("body", req.body);
+      }
+
+      // case where responding to a REST call sent to tropo
+      if (req.body
+          && req.body.session
+          && req.body.session.parameters
+          && req.body.session.parameters.start_conversation
+          && req.body.session.parameters.send_to
+          && req.body.session.parameters.message)
+      {
+        // use number to send_to as chatid
+        let phone_number = req.body.session.parameters.send_to;
+        let chatId = phone_number;
+        let messageText = req.body.session.parameters.message;
+
+        console.log("Attempting to initiate text: ", phone_number,
+                    ", api.ai message: ", messageText);
+
+        if (!this._sessionIds.has(chatId)) {
+            this._sessionIds.set(chatId, uuid.v1());
         }
 
-        if (req.body && req.body.session && req.body.session.from && req.body.session.initialText) {
-            let chatId = req.body.session.from.id;
-            let messageText = req.body.session.initialText;
+        let apiaiRequest = this._apiaiService.textRequest(
+          messageText,
+            { sessionId: this._sessionIds.get(chatId) });
 
-            console.log(chatId, messageText);
+        apiaiRequest.on('response', (response) => {
+          if (TropoBot.isDefined(response.result)) {
+            console.log("api_ai response: ", response)
+            let responseText = response.result.fulfillment.speech;
 
-            if (messageText) {
-                if (!this._sessionIds.has(chatId)) {
-                    this._sessionIds.set(chatId, uuid.v1());
-                }
+            if (TropoBot.isDefined(responseText)) {
+              console.log('Initiate text message');
 
-                let apiaiRequest = this._apiaiService.textRequest(messageText,
-                    {
-                        sessionId: this._sessionIds.get(chatId)
-                    });
+              return res.status(200).json({
+                tropo: [
+                  {call:
+                   {
+                     to: phone_number,
+                     network: "SMS",
+                   }
+                  },
+                  {say:{value: responseText}}
+                ]});
 
-                apiaiRequest.on('response', (response) => {
-                    if (TropoBot.isDefined(response.result)) {
-                        let responseText = response.result.fulfillment.speech;
+            } else {
+              console.log('Received empty speech');
+              return res.status(400).end('Received empty speech');
+            }
+          } else {
+            console.log('Received empty result');
+            return res.status(400).end('Received empty result');
+          }
+        });
 
-                        if (TropoBot.isDefined(responseText)) {
-                            console.log('Response as text message');
+        apiaiRequest.on('error', (error) => {
+          console.error(error)
+          return res.status(400).end('Internal Error');
+        });
+        apiaiRequest.end();
+      }
 
-                            res.status(200).json({
-                                tropo: [{say: {value: responseText}}]
-                            });
+      // case where receiving an incoming message
+      else if (req.body
+               && req.body.session
+          && req.body.session.from
+          && req.body.session.initialText)
+      {
+        let chatId = req.body.session.from.id;
+        let messageText = req.body.session.initialText;
 
-                        } else {
-                            console.log('Received empty speech');
-                            return res.status(400).end('Received empty speech');
-                        }
-                    } else {
-                        console.log('Received empty result');
-                        return res.status(400).end('Received empty result');
-                    }
+        console.log(chatId, messageText);
+
+        if (messageText) {
+          if (!this._sessionIds.has(chatId)) {
+            this._sessionIds.set(chatId, uuid.v1());
+          }
+
+          let apiaiRequest = this._apiaiService.textRequest(
+            messageText,
+            { sessionId: this._sessionIds.get(chatId) });
+
+          apiaiRequest.on('response', (response) => {
+            if (TropoBot.isDefined(response.result)) {
+              let responseText = response.result.fulfillment.speech;
+
+              if (TropoBot.isDefined(responseText)) {
+                console.log('Response as text message');
+
+                return res.status(200).json({
+                  tropo: [{say: {value: responseText}}]
                 });
 
-                apiaiRequest.on('error', (error) => console.error(error));
-                apiaiRequest.end();
+              } else {
+                console.log('Received empty speech');
+                return res.status(400).end('Received empty speech');
+              }
+            } else {
+              console.log('Received empty result');
+              return res.status(400).end('Received empty result');
             }
-            else {
-                console.log('Empty message');
-                return res.status(400).end('Empty message');
-            }
+          });
+
+          apiaiRequest.on('error', (error) => {
+            console.error(error)
+            return res.status(400).end('Internal Error');
+          });
+          apiaiRequest.end();
         } else {
-            console.log('Empty message');
-            return res.status(400).end('Empty message');
+          // XX (mtourne): seems like the first if takes care of this
+          console.log('Empty message');
+          return res.status(400).end('Empty message');
         }
+      } else {
+        console.log('Empty message');
+        return res.status(400).end('Empty message');
+      }
     }
 
     static isDefined(obj) {
